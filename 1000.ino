92863ffaf4540a78a2f817d1cfc97c7302283698
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-21 20:04:27 +0600

    Blink LED

---
+++ 1000.ino
@@ -0,0 +1,15 @@
+/* A sample program for blink a LED for a specific amount of time. */
+
+void setup(){
+
+    pinMode(2,OUTPUT);
+    Serial.begin(9600);
+
+}
+
+void loop(){
+    digitalWrite(2,HIGH);
+    delay(1000);
+    digitalWrite(2,LOW);
+    delay(1000);
+}
