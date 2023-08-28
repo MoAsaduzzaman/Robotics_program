f6a0fc53cd7916ef88819578be618610e96f98fd
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-08-28 18:54:27 +0600

    Controlling Brightness of LED

---
+++ 1007.ino
@@ -0,0 +1,31 @@
+/*A sample program for Controlling Brightness of LED*/
+
+int led=6;
+
+void setup(){
+    pinMode(led,OUTPUT);
+
+}
+
+void loop(){
+    analogWrite(led,10);
+    delay(1000);
+    analogWrite(led,225);
+    delay(1000);
+}
+
+/*Another way for this program using for loop*/
+
+int led=6;
+
+void setup(){
+    pinMode(led,OUTPUT);
+
+}
+
+void loop(){
+    for(int i=0; i<=255; i+=10;){
+        analogWrite(led,i);
+        delay(5000);
+    }
+}
